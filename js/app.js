7e3ed47b32a3dd9bca575e619c5b1af3079f945a
My Name <abirwww770@gmail.com>
2024-06-05 12:20:25 +0600

    complete banner and top product

---
+++ js/app.js
@@ -0,0 +1,26 @@
+window.Alpine = Alpine;
+
+let search_keywords = [
+    "MacBook Pro ",
+    "AirPods Pro",
+    "Samsung S9",
+    "Tablet",
+    "Xiaomi",
+    "JBL speaker",
+    "Canon",
+    "AirPods Max",
+    "Asus",
+    "MagSafe",
+]
+
+function increment_cart(){
+    event.preventDefault();
+    let input = event.currentTarget.previousElementSibling;
+    input.value = parseInt(input.value) + 1;
+}
+
+function decrement_cart(){
+    event.preventDefault();
+    let input = event.currentTarget.nextElementSibling;
+    input.value = parseInt(input.value) - 1;
+}
