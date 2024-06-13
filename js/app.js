5e09586881e1b5416dad46c1ae5ac6b94b356598
My Name <abirwww770@gmail.com>
2024-06-13 15:42:22 +0600

    xs responsive complete

--- js/app.js
+++ js/app.js
@@ -27,3 +27,10 @@ function decrement_cart(){
     let input = event.currentTarget.nextElementSibling;
     input.value = parseInt(input.value) - 1;
 }
+
+function toggle_nav(){
+    let nav_bar_menue = document.getElementById('nav_bar_menue');
+    if(nav_bar_menue){
+        nav_bar_menue.classList.toggle('active');
+    }
+}
